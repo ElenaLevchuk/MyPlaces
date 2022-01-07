7f84a219011aaef52115841c54c8c80083b33de5
Elena Levchuk <yogalize@gmail.com>
2022-01-07 20:24:45 +0200

    Some appear improvement App icon Launch screen Cell height Image rounded corners

--- MyPlaces/MainTableViewController.swift
+++ MyPlaces/MainTableViewController.swift
@@ -34,10 +34,16 @@ class MainTableViewController: UITableViewController {
                                                  for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
     }
     
-
+// MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
     
     // MARK: - Navigation
 
